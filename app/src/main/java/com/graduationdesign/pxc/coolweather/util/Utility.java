88dfa96a97bded6b72e579595b66fbdf94fbaa34
Calvert <xchaha@foxmail.com>
2018-02-13 23:07:10 +0800

    实现查询天气无图版

--- app/src/main/java/com/graduationdesign/pxc/coolweather/util/Utility.java
+++ app/src/main/java/com/graduationdesign/pxc/coolweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.graduationdesign.pxc.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.graduationdesign.pxc.coolweather.db.City;
 import com.graduationdesign.pxc.coolweather.db.County;
 import com.graduationdesign.pxc.coolweather.db.Province;
+import com.graduationdesign.pxc.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -83,4 +85,17 @@ public class Utility {
         return false;
 
     }
+
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
